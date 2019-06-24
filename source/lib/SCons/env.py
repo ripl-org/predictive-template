d726fc70cae4b61ec27173311041ec082aceaf62
Mark Howison <mhowison@brown.edu>
2019-06-24 12:14:16 -0400

    added stata-addons2 path into SCons env

--- source/lib/SCons/env.py
+++ source/lib/SCons/env.py
@@ -3,17 +3,18 @@ import getpass
 
 repo_root = Dir(".").abspath
 varnames = ["STATATMP", "PYTHON_DIR", "MATLAB_ROOT",
-            "USER", "HOME", "PATH","LOGNAME", "LANG", 
-            "LD_LIBRARY_PATH","LD_RUN_PATH","LIBRARY_PATH", 
+            "USER", "HOME", "PATH", "LOGNAME", "LANG", 
+            "LD_LIBRARY_PATH", "LD_RUN_PATH", "LIBRARY_PATH", 
             "MODULEHOME", "MODULEPATH", "MODULESHELL", "MODULEPYTHON", 
             "QTDIR", "QTLIB", "TERM", "SHELL", "DISPLAY", "ORACLE_HOME", 
-            "LOADEDMODULES", "S_ADO", "SHLVL","PKG_CONFIG_PATH", "TWO_TASK"]
+            "LOADEDMODULES", "SHLVL", "PKG_CONFIG_PATH", "TWO_TASK"]
 
 env_vars = {var: os.environ.get(var, "") for var in varnames}
 env_vars.update({
     "REPO_ROOT": repo_root,
     "PYTHONPATH": os.path.abspath("./source/lib/Python"),
-    "ORACLE_SID": "RIPL1",
+    "S_ADO": "BASE;{};/data/stata-addons2;{}".format(os.path.abspath("./source/lib/Stata/gslab_misc/ado"),
+                                                     os.environ.get("S_ADO", "").replace("BASE;", "")),
     "R_LIBS": "",
     "R_LIBS_USER": ""
 })
